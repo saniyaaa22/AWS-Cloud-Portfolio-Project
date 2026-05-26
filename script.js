7e23ffea774f87603faf623998e8fd570543c38a
Saniyaaa22 <saniyaupadhyay04@gmail.com>
2026-05-26 17:04:45 +0530

    Added portfolio site

---
+++ script.js
@@ -0,0 +1,9 @@
+function showAlert() {
+    alert("Welcome to my Cloud Portfolio 🚀");
+}
+
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({
+        behavior: "smooth"
+    });
+}
